aa30d1c530312e6a069c6541205ffc31cf349f7f
ItsShivamsaurav <149001568+ItsShivamsaurav@users.noreply.github.com>
2025-02-18 01:06:44 +0530

    Update gallery.js
    added toogle button.

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,30 +1,14 @@
-const openVideoSection = document.getElementById('openVideoSection');
-const openImageSection = document.getElementById('openImageSection');
-document.getElementById('openImageSection').addEventListener('click', function() {
+
+document.getElementById('switchCheckbox').addEventListener('change', function() {
     var imageSection = document.getElementById('imageSection');
-    var videoSection = document.getElementById('videoSection');
-
-    if (imageSection.classList.contains('hidden')) {
-        imageSection.classList.remove('hidden');
-        openImageSection.classList.add('hidden');
-        videoSection.classList.add('hidden');
-        openVideoSection.classList.remove('hidden');
-    }
-});
-
-document.getElementById('openVideoSection').addEventListener('click', function() {
-    var imageSection = document.getElementById('imageSection');
-    var videoSection = document.getElementById('videoSection');
-
-   
-    if (videoSection.classList.contains('hidden')) {
+     var videoSection = document.getElementById('videoSection');
+    if (this.checked) {
         videoSection.classList.remove('hidden');
-        openVideoSection.classList.add('hidden');
         imageSection.classList.add('hidden');
-        openImageSection.classList.remove('hidden');
+    } else {
+        imageSection.classList.remove('hidden');
+        videoSection.classList.add('hidden');
+        
     }
-});
-
-document.getElementById('openVideoSection').classList.remove('hidden');
-document.getElementById('imageSection').classList.remove('hidden');
-document.getElementById('videoSection').classList.add('hidden');
+  });
+  
